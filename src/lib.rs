823c22a340dad4b032aa138ff2bc7904dac0da85
saroj-2728 <080bct075.saroj@pcampus.edu.np>
2025-07-20 02:57:31 +0545

    feat: implement basic search functionality with tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,34 @@ impl Args {
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     let file_contents = fs::read_to_string(args.file_path)?;
 
-    println!("File content: {}", file_contents);
+    for line in search(&args.query, &file_contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
